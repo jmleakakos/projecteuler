f722cffcff2214d549fde0232c64f952ef28638a
Jeremy Leakakos <nimbus57@gmail.com>
2017-05-29 15:35:07 -0600

    Start using Data.Numbers.Primes instead of a custom prime function

--- haskell/003.hs
+++ haskell/003.hs
@@ -4,19 +4,24 @@
 -- 
 -- What is the largest prime factor of the number 600851475143 ?
 
-import Data.List
+import Data.Numbers.Primes
 
-isPrime :: Int -> Bool
-isPrime 1 = False
-isPrime 2 = True
-isPrime 3 = True
-isPrime n 
-  | n `mod` 2 == 0 = False
-  | otherwise      = not $ any (\x -> n `mod` x == 0) [x | x <- [3..upperLimit], odd x]
-  where upperLimit = floor $ sqrt (fromIntegral n)
+main = (print . last) $ primeFactors 600851475143
 
-primes = 2 : [x | x <- [3..], odd x, isPrime x]
-
-main = print $ find (\x -> v `mod` x == 0) ps
-  where ps = reverse $ takeWhile (< (floor $ sqrt (fromIntegral v))) primes
-        v = 600851475143
+-- Initially done using a custom isPrime function
+-- import Data.List
+--
+-- isPrime :: Int -> Bool
+-- isPrime 1 = False
+-- isPrime 2 = True
+-- isPrime 3 = True
+-- isPrime n
+--   | n `mod` 2 == 0 = False
+--   | otherwise      = not $ any (\x -> n `mod` x == 0) [x | x <- [3..upperLimit], odd x]
+--   where upperLimit = floor $ sqrt (fromIntegral n)
+--
+-- primes = 2 : [x | x <- [3..], odd x, isPrime x]
+--
+-- main = print $ find (\x -> v `mod` x == 0) ps
+--   where ps = reverse $ takeWhile (< (floor $ sqrt (fromIntegral v))) primes
+--         v = 600851475143
