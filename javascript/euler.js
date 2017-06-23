ddcc9a1a8f95ff18cf688c545d35b964976d0297
Jeremy Leakakos <nimbus57@gmail.com>
2017-06-23 12:19:07 -0600

    Problem 6 javascript

--- javascript/euler.js
+++ javascript/euler.js
@@ -168,3 +168,28 @@ function factorCount(number, factor) {
   }
   return count;
 }
+
+/*
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+function problem006() {
+  var l = [];
+  for(var n = 1; n <= 100; n++) {
+    l.push(n);
+  }
+  var sumOfSquares = l.reduce(function(acc, n) {
+    return acc + n * n
+  }, 0);
+  var squareOfSums = l.reduce(function(acc, n) {
+    return acc + n;
+  }, 0) ** 2;
+  console.log("Problem 006: " + (squareOfSums - sumOfSquares));
+}
