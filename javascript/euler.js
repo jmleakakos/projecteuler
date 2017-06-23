7817922b9bb5393b366ca65be72a0af1a1f23c8e
Jeremy Leakakos <nimbus57@gmail.com>
2017-06-23 14:14:23 -0600

    Formatted file text

--- javascript/euler.js
+++ javascript/euler.js
@@ -93,7 +93,7 @@ function primes(n = 2) {
   // setup all numbers
   // mark all wheel-based intervals
   for (var i = 2; i <= LIMIT; i++) {
-    if(wheel.includes(i)) {
+    if (wheel.includes(i)) {
       numbersAndMarks.push([i, false])
     } else if (wheel.some(function (prime) { return i % prime === 0 })) {
       numbersAndMarks.push([i, true]);
@@ -107,14 +107,14 @@ function primes(n = 2) {
     if (!marked) {
       // iterate over multiples
       for (var j = number + number; j < numbersAndMarks.length; j += number) {
-        numbersAndMarks[j-1] = [j, true];
+        numbersAndMarks[j - 1] = [j, true];
       }
     }
   }
   var primes = [];
   var markedPrimes = numbersAndMarks.forEach(function (nm) {
     var [number, marked] = nm;
-    if(!marked) {
+    if (!marked) {
       primes.push(number);
     }
   });
@@ -154,7 +154,7 @@ What is the smallest positive number that is evenly divisible by all of the numb
 function problem005() {
   var ps = primes(20);
   var primeFactors = {};
-  ps.map(function(p) {
+  ps.map(function (p) {
     primeFactors[p] = 0;
   });
   const START = 2;
@@ -163,12 +163,12 @@ function problem005() {
     for (var psIndex = 0; psIndex < ps.length; psIndex++) {
       var prime = ps[psIndex];
       var count = factorCount(num, prime);
-      if(count > 0 && primeFactors[prime] < count) {
+      if (count > 0 && primeFactors[prime] < count) {
         primeFactors[prime] = count;
       }
     }
   }
-  var lcm = ps.reduce(function(acc, prime) {
+  var lcm = ps.reduce(function (acc, prime) {
     return acc * prime ** primeFactors[prime]
   }, 1);
   console.log("Problem 005: " + lcm);
@@ -176,10 +176,10 @@ function problem005() {
 
 function factorCount(number, factor) {
   var count = 0;
-  if(number % factor === 0) {
+  if (number % factor === 0) {
     count++;
     var result = number / factor;
-    while(result % factor === 0) {
+    while (result % factor === 0) {
       count++;
       result = result / factor;
     }
@@ -200,13 +200,13 @@ Find the difference between the sum of the squares of the first one hundred natu
 */
 function problem006() {
   var l = [];
-  for(var n = 1; n <= 100; n++) {
+  for (var n = 1; n <= 100; n++) {
     l.push(n);
   }
-  var sumOfSquares = l.reduce(function(acc, n) {
+  var sumOfSquares = l.reduce(function (acc, n) {
     return acc + n * n
   }, 0);
-  var squareOfSums = l.reduce(function(acc, n) {
+  var squareOfSums = l.reduce(function (acc, n) {
     return acc + n;
   }, 0) ** 2;
   console.log("Problem 006: " + (squareOfSums - sumOfSquares));
@@ -226,15 +226,15 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 */
 function problem008() {
   const numberAsString = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-  var numbers = numberAsString.split('').map(function(n) { return parseInt(n) });
+  var numbers = numberAsString.split('').map(function (n) { return parseInt(n) });
   const windowSize = 13;
   var greatestProduct = 0;
-  for(var index = 0; index < numbers.length - windowSize; index++) {
+  for (var index = 0; index < numbers.length - windowSize; index++) {
     var window = numbers.slice(index, index + windowSize);
-    var product = window.reduce(function(acc, n) {
+    var product = window.reduce(function (acc, n) {
       return acc * n;
     }, 1);
-    if(product > greatestProduct) {
+    if (product > greatestProduct) {
       greatestProduct = product;
     }
   }
@@ -276,6 +276,6 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 function problem010() {
-  var sum = primes(2000000).reduce(function(acc, n) { return acc + n });
+  var sum = primes(2000000).reduce(function (acc, n) { return acc + n });
   console.log("Problem 010: " + sum);
 }
